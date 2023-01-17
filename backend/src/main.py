8f101c498eea909d355b0d3680917a74fc3f9cf0
wesson <vinokurov.filipp1@yandex.ru>
2023-01-17 04:30:42 +0300

    added prometheus + grafana to app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import PrometheusFastApiInstrumentator
 import backend.src.auth.router as auth
 from backend.src.budget.routers import account, income, spending
 from backend.src.config import DEFAULT_API_PREFIX
@@ -10,3 +11,15 @@ app.include_router(auth.router, prefix=f'{DEFAULT_API_PREFIX}', tags=['Users'])
 app.include_router(income.router, prefix=f'{DEFAULT_API_PREFIX}/budget', tags=['Incomes'])
 app.include_router(account.router, prefix=f'{DEFAULT_API_PREFIX}/budget', tags=['Accounts'])
 app.include_router(spending.router, prefix=f'{DEFAULT_API_PREFIX}/budget', tags=['Spendings'])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+PrometheusFastApiInstrumentator(
+    excluded_handlers=["/metrics"]
+).instrument(app).expose(app)

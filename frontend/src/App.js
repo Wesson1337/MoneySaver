4a145fc887b319565251b41974802934947d662c
wesson <vinokurov.filipp1@yandex.ru>
2023-02-09 05:46:53 +0300

    made project structure, changed alpine version of frontend from 3.17 to 3.16 due to error with docker-entrypoint.sh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ClassCounter from "./components/ClassCounter";
 
 function App() {
   return (
     <div className="App">
-        <ClassCounter />
+        WELCOME TO APP
     </div>
   );
 }

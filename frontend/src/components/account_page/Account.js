47caba33cdc29fb9201c10545c2f8298f8663e6f
wesson <vinokurov.filipp1@yandex.ru>
2023-04-28 02:56:30 +0300

    made expand logic in Account.js

--- frontend/src/components/account_page/Account.js
+++ frontend/src/components/account_page/Account.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card, Form} from "react-bootstrap";
-import {CURRENCIES_AND_SYMBOLS} from "../../utils/consts";
+import {ACCOUNT_TYPES, CURRENCIES_AND_SYMBOLS} from "../../utils/consts";
 import {prettifyFloat} from "../../utils/prettifyFloat";
 import transferIcon from "../../static/icons/transfer-svgrepo-com.svg"
 import editIcon from "../../static/icons/edit-svgrepo-com.svg"
 import eyeIcon from "../../static/icons/eye-open-svgrepo-com.svg"
 
 const Account = ({account, index}) => {
+    const [isExpanded, setIsExpanded] = useState(false)
+
 
     const handleCheckOnChange = async () => {
 
@@ -15,6 +17,10 @@ const Account = ({account, index}) => {
     return (
         <Card
             className="p-3 d-flex flex-column gap-2"
+            style={{
+                height: isExpanded ? "135px" : "99.34px",
+                transition: "0.25s ease-out"
+            }}
         >
             <div className="d-flex justify-content-between align-items-center">
                 <p className="m-0">{account.name}</p>
@@ -29,10 +35,13 @@ const Account = ({account, index}) => {
                         defaultChecked={account["is_active"]}
                     />
                 </Form>
-                <div className="d-flex gap-2">
+                <div
+                    className="d-flex gap-2"
+                >
                     <div
                         className="account-action"
-                        style={{
+                        onClick={() => {
+                            setIsExpanded(!isExpanded)
                         }}
                     >
                         <img
@@ -60,6 +69,16 @@ const Account = ({account, index}) => {
                         />
                     </div>
                 </div>
+            </div>
+            <div className="d-flex justify-content-between mt-1">
+                <p
+                    className="m-0"
+                    style={{transition: "0.15s ease-out", color: isExpanded ? "black" : "transparent"}}
+                >Type: {ACCOUNT_TYPES[account["type"]].name}</p>
+                <p
+                    className="m-0"
+                    style={{transition: "0.15s ease-out", color: isExpanded ? "black" : "transparent"}}
+                >Balance in USD: {prettifyFloat(account["balanceInUSD"].toFixed(2))} $</p>
             </div>
         </Card>
     );

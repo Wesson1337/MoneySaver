ff4b4cff4abebc609e8feb67e05b7c2136ef274f
wesson <vinokurov.filipp1@yandex.ru>
2023-05-03 01:48:30 +0300

    finished styling EditModal.js

--- frontend/src/components/account_page/EditModal.js
+++ frontend/src/components/account_page/EditModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Modal} from "react-bootstrap";
+import {Button, Col, Form, Modal, Row} from "react-bootstrap";
+import Select from "react-select";
 
 const EditModal = ({show, setShow}) => {
     const handleOnHide = () => {
@@ -10,8 +11,36 @@ const EditModal = ({show, setShow}) => {
         <Modal
             show={show}
             onHide={handleOnHide}
+            size="lg"
         >
-
+            <Modal.Header closeButton>
+                <Modal.Title>Edit account</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Row>
+                        <Form.Group as={Col}>
+                            <Form.Label className="little-text mb-1">Name</Form.Label>
+                            <Form.Control
+                                maxLength={255}
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Label className="little-text mb-1">Type</Form.Label>
+                            <Select
+                                
+                            />
+                        </Form.Group>
+                    </Row>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant="secondary"
+                    onClick={handleOnHide}
+                >Close</Button>
+                <Button>Save</Button>
+            </Modal.Footer>
         </Modal>
     );
 };
